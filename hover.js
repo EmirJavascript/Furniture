017dcb62762b1fc8efde9c1f42610a232a26d459
beko099 <begali09092004@gmail.com>
2023-04-18 19:05:07 +0600

    add animation

---
+++ hover.js
@@ -0,0 +1,12 @@
+// const photo1 = document.querySelector('.photo1')
+
+// photo1.addEventListener('mouseover', () => {
+//     // photo1.setAttribute('src', 'https://htmldemo.net/artfurniture/artfurniture/assets/img/product/6.jpg')
+//     photo1.style.opacity = '0'
+// })
+
+// photo1.addEventListener('mouseout', () => {
+//     // photo1.setAttribute('src', 'https://htmldemo.net/artfurniture/artfurniture/assets/img/widget/1.jpg')
+//     photo1.style.opacity = '1'
+
+// })
